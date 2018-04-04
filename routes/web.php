08f5c8f1bbc05e8137382dca41f94a7f30923e7f
lejvincent <lej.vincent26@gmail.com>
2018-04-04 15:12:15 +0200

    javais oublié une route lol

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 Auth::routes();
 
-Route::get('index', 'HomeController@index')->name('front_index');
+Route::get('index', 'HomeController@index')->name('front_categories');
 Route::get('settings', 'HomeController@settings')->name('front_settings');
 Route::get('profil', 'HomeController@profil')->name('front_profil');
 Route::get('home', 'HomeController@home')->name('front_accueil');
