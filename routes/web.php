3ba1f4ac2dfc09b1c62353e5402b7b8f561323e6
roms07 <ribetromain@yahoo.fr>
2018-04-17 12:13:12 +0200

    add CatController and change name index to categories

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 Auth::routes();
 
 Route::get('/', 'HomeController@home')->name('front_accueil');
-Route::get('index', 'HomeController@index')->name('front_categories');
+Route::get('categories', 'CatController@categories')->name('front_categories');
 Route::get('settings', 'HomeController@settings')->name('front_settings');
 Route::get('profil', 'HomeController@profil')->name('front_profil');
 Route::get('home', 'HomeController@home')->name('front_accueil');
