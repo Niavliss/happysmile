60f35e3bbcf84b09d11973a2e9ec602b0ce9a684
MelBou <bouzon.melissa@gmail.com>
2018-04-19 15:26:10 +0200

    Publish and profile pages updated

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,29 +6,8 @@
     <div id="cont_fiche_profil"></div>
     <div id="menu_profil">
         <img class="img-fluid" src="{{URL::asset('img/user_logo.png')}}">
-        <h2>Pseudo</h2>
-        <ul>
-            <li><button class="btn btn-warning btn-sm" data-toggle="modal" data-target="#modal_publier">Publier</button>
-                <div class="modal fade" id="modal_publier" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLongTitle">Publication</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <textarea name="textarea" rows="5" cols="48"></textarea>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-dark" data-dismiss="modal">Fermer</button>
-                                <button type="button" class="btn btn-warning">Publier</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </li>
+        <h2>{{Auth::user()->name}}</h2>
+        <ul><a href="/publier"><button class="btn btn-warning btn-sm">Publier</button></a>
             <li><button class="btn btn-warning btn-sm">Amis</button></li>
             <li><button class="btn btn-warning btn-sm">Messages</button></li>
             <li><button class="btn btn-warning btn-sm">Demander en amis</button></li>
--- resources/views/publish.blade.php
+++ resources/views/publish.blade.php
@@ -6,7 +6,7 @@
     <div id="cont_fiche_profil"></div>
     <div id="menu_profil">
         <img class="img-fluid" src="{{URL::asset('img/user_logo.png')}}">
-        <h2>Pseudo</h2>
+        <h2>{{Auth::user()->name}}</h2>
 
     </div>
     <div class="container" id="publish_body_profil">
@@ -15,6 +15,11 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Publier une blague :</h5>
+
+
+                        <ul><a href="/profil"><button class="btn btn-warning btn-sm">Retour</button></a>
+                            <button class="btn btn-warning btn-sm">Publier</button>
+                        </ul>
                     </div>
                 </div>
             </div>
