4644b4aa3d387029dbc164c6e91d3c18ddddbde6
roms07 <ribetromain@yahoo.fr>
2018-04-25 14:23:58 +0200

    debug route publier

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ Route::get('/categories/publier', 'PostController@create')->name('front_categori
 Route::post('/categories/publier', 'PostController@storePrivate')->name('front_categories_store');
 Route::get('/mon-profil', 'UserController@myprofile')->name('front_profile');
 Route::get('/profil/{id}', 'UserController@profile')->name('front_profile_show');
-Route::get('/profil/publier', 'PostController@create')->name('front_profile_publish');
-Route::post('/profil/publier', 'PostController@store')->name('front_profile_store');
+Route::get('/publier', 'PostController@create')->name('front_profile_publish');
+Route::post('/publier', 'PostController@store')->name('front_profile_store');
 Route::get('/parametres', 'UserController@settings')->name('front_settings');
 Route::post('/profil', 'UserController@uploadImg')->name('upload_image');
 Route::get('/membres', 'UserController@members')->name('front_members');
-Route::get('/publier', 'UserController@publish')->name('front_profile_publish');
+//Route::get('/publier', 'UserController@publish')->name('front_profile_publish');
 
 // Route::get('/profil/{id}/message', 'HomeController@index')->name('amis');
 // Route::post('/profil/{id}/message', 'HomeController@index')->name('amis');
