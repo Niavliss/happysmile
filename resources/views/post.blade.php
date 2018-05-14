a0c4b97ec067fc61ed16413706c945661bdc16cc
Niavliss <sylvain.felix@gmail.com>
2018-05-14 16:13:52 +0200

    modified  post view  first version

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,13 +5,15 @@
 
 
 @section('content')
-    <div class="container" id="cont_body_categories">
+    <div class="container" id="post">
         <div class="row">
             <div class="col-12">
-                <div class="card txt">
+                <div class="card txt post">
                     <div class="card-body">
-                        <h5 class="card-title">{{ $post->title }}</h5>
-                        <p class="card-text">{{ $post->content }} </p>
+                        <img class="imgprofil" src="{{ asset('storage/' . $post->user->pic_path) }}">
+                        <a href="{{ url('profil/'.$post->user->id) }}">{{$post->user->name}}</a>
+                        <h5 class="card-title titlepost badge badge-warning mr-2 ">{{ $post->title }}</h5>
+                        <p class="card-text content">{{ $post->content }} </p>
                     </div>
                 </div>
             </div>
